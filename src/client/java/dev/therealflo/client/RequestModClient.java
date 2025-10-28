3d25483d8a463f77b257184656dab68f141d5034
Florian Reintgen <85225228+TheReal-Flo@users.noreply.github.com>
2025-10-28 01:22:20 +0100

    Initial first attempt of intercepting keys

---
+++ src/client/java/dev/therealflo/client/RequestModClient.java
@@ -0,0 +1,10 @@
+package dev.therealflo.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RequestModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/therealflo/RequestMod.java
@@ -0,0 +1,10 @@
+package dev.therealflo;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RequestMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
